f23f50607aa94b6a0889953796fc2a25be7a5b99
xGinDev <jecl2912@gmail.com>
2025-02-04 21:50:14 -0500

    fix: test more snippets cli

--- cli/setup-aliases.ps1
+++ cli/setup-aliases.ps1
@@ -1,5 +1,7 @@
 # Agregar aliases al perfil de PowerShell
 Add-Content -Path $PROFILE -Value "Set-Alias -Name vl -Value vtex link"
+Add-Content -Path $PROFILE -Value "Set-Alias -Name vli -Value vtex login"
+Add-Content -Path $PROFILE -Value "Set-Alias -Name vlo -Value vtex logout"
 Add-Content -Path $PROFILE -Value "Set-Alias -Name yd -Value yarn dev"
 
 # Recargar el perfil de PowerShell
--- cli/setup-aliases.sh
+++ cli/setup-aliases.sh
@@ -2,6 +2,8 @@
 
 # Agregar aliases al archivo de configuración de la terminal
 echo 'alias vl="vtex link"' >> ~/.zshrc
+echo 'alias vli="vtex login"' >> ~/.zshrc
+echo 'alias vlo="vtex logout"' >> ~/.zshrc
 echo 'alias yd="yarn dev"' >> ~/.zshrc
 
 # Recargar la configuración de la terminal
